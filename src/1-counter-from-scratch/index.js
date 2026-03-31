ee53e9f3bc24ab5fa27d03277561707f7d514b69
[usherissleepy] <[ashar.zarrar218@gmail.com]>
2026-03-30 23:34:51 -0400

    counter from scratch completed

---
+++ src/1-counter-from-scratch/index.js
@@ -0,0 +1,15 @@
+let count = 0;
+
+const countDisplay = document.getElementById("count")
+const incrementButton = document.getElementById("increment")
+const resetButton = document.getElementById("reset")
+
+incrementButton.addEventListener("click", () => {
+    count++
+    countDisplay.textContent = count;
+})
+
+resetButton.addEventListener("click", () => {
+    count = 0;
+    countDisplay.textContent = count;
+})
